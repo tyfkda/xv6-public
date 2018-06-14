cc00f9fb977e8f498442446b98d440745c9b246f
tyfkda <tyfkda@gmail.com>
2018-06-14 15:04:22 +0900

    Fix open to accept more arguments

--- include/user.h
+++ include/user.h
@@ -11,7 +11,7 @@ int read(int, void*, int);
 int close(int);
 int kill(int);
 int exec(char*, char**);
-int open(const char*, int);
+int open(const char*, int, ...);
 int mknod(const char*, short, short);
 int unlink(const char*);
 int fstat(int fd, struct stat*);
