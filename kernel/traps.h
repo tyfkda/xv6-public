5b0ce99b85f55f0a71ef64f10a97524c6438b8f3
tyfkda <tyfkda@gmail.com>
2018-06-15 16:04:51 +0900

    Set syscall number as $80
    Same as 32bit Linux.

--- kernel/traps.h
+++ kernel/traps.h
@@ -24,7 +24,7 @@
 
 // These are arbitrarily chosen, but with care not to overlap
 // processor defined exceptions or interrupt vectors.
-#define T_SYSCALL       64      // system call
+#define T_SYSCALL      128      // system call
 #define T_DEFAULT      500      // catchall
 
 #define T_IRQ0          32      // IRQ 0 corresponds to int T_IRQ
@@ -35,4 +35,3 @@
 #define IRQ_IDE         14
 #define IRQ_ERROR       19
 #define IRQ_SPURIOUS    31
-
