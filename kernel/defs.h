b3626622710655efef7f19de0cefb62670a79723
tyfkda <tyfkda@gmail.com>
2018-06-15 11:50:58 +0900

    Specify exit function as noreturn

--- kernel/defs.h
+++ kernel/defs.h
@@ -104,7 +104,7 @@ int             pipewrite(struct pipe*, char*, int);
 //PAGEBREAK: 16
 // proc.c
 int             cpuid(void);
-void            exit(int);
+void            exit(int) __attribute__((noreturn));
 int             fork(void);
 int             growproc(int);
 int             kill(int);
